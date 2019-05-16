6e602e1d62a702c4b69776463788d76a46c01fe3
I346501 <bob.vanvelzen@student.fontys.nl>
2019-05-16 12:02:12 +0200

    added basic note setup

---
+++ Amnesty International Group 2/Assets/Scripts/NoteManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NoteManager : ScriptableObject
+{
+    public List<Note> Notes = new List<Note>();
+}
