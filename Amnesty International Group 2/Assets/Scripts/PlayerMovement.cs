3ef15eff2ce5ae7e5b6c74148c5aebafc119c50d
BobiGeorge <boris.georg98@gmail.com>
2019-05-16 13:05:02 +0200

    Player movement + Grid added

---
+++ Amnesty International Group 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    
+    private Rigidbody2D playerRigidbody;
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        playerRigidbody.MovePosition(playerRigidbody.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
