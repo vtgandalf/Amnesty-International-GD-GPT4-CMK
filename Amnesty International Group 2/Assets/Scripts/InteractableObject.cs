31262f0a12a384393b5702bbca8205074efbe980
BobiGeorge <boris.georg98@gmail.com>
2019-05-17 11:49:52 +0200

    Interactive objects added

---
+++ Amnesty International Group 2/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        Debug.Log("Hello, i am Mage Bob");
+    }
+}
--- Amnesty International Group 2/Assets/Scripts/PlayerMovement.cs
+++ Amnesty International Group 2/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    
     private Rigidbody2D playerRigidbody;
+
+    private InteractableObject objectInRangeScript = null;
+   
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
     }
-
 
     void FixedUpdate()
     {
@@ -19,5 +20,28 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxisRaw("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         playerRigidbody.MovePosition(playerRigidbody.position + movement * speed * Time.fixedDeltaTime);
+
+        if (Input.GetKeyDown(KeyCode.E) && objectInRangeScript != null)
+        {
+            objectInRangeScript.Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter");
+        if (other.CompareTag("Interactable"))
+        {
+            objectInRangeScript = other.GetComponent<InteractableObject>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == objectInRangeScript.gameObject)
+        {
+            Debug.Log("exit");
+            objectInRangeScript = null;
+        }
     }
 }
