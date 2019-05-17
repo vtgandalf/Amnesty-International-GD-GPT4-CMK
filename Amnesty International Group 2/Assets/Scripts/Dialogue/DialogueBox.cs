c5ff4a33af35a658a05f71b58bf30caa9e599511
Valeri Todorov <37157239+vtgandalf@users.noreply.github.com>
2019-05-17 13:14:48 +0200

    Basic Dialogue System
    1.0 version is done

---
+++ Amnesty International Group 2/Assets/Scripts/Dialogue/DialogueBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+    public Text DialogueText;
+    public Text NameText;
+    public TextHandler handler;
+    Dialogue dialogue;
+    int position = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = handler.LoadDialogue();
+        LoadNextMessage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextMessage(){
+        if (position < dialogue.messages.Length){
+            Message message = handler.LoadText(position);
+            NameText.text = dialogue.charactersInfo[message.charId].name;
+            DialogueText.text = message.text;
+            position++;
+        }else{
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Amnesty International Group 2/Assets/Scripts/Dialogue/DialogueData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName ="Dialogue/New Dialogue")]
+public class DialogueData : Dialogue
+{
+    
+}
