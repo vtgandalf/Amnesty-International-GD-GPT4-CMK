065f6bff3e81cb0261dee323a3061f97fadaf056
I346501 <bob.vanvelzen@student.fontys.nl>
2019-05-23 10:49:09 +0200

    Player sorting and camera movement

---
+++ Amnesty International Group 2/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform Target;
+    public Vector3 TargetOffset;
+    public float smoothTime;
+    public float maxSpeed;
+
+    private Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null)
+            transform.position = Vector3.SmoothDamp(transform.position, Target.position + TargetOffset, ref velocity, smoothTime, maxSpeed);
+    }
+}
