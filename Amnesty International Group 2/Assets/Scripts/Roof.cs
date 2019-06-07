1a15783fbb34d8d8476c70027f55f7629770a339
I346501 <bob.vanvelzen@student.fontys.nl>
2019-06-07 15:03:41 +0200

    Added Mosque level

---
+++ Amnesty International Group 2/Assets/Scripts/Roof.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Roof : MonoBehaviour
+{
+    [SerializeField] private List<Renderer> rends;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && rends.Count > 0)
+        {
+            foreach (Renderer r in rends)
+                r.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && rends.Count > 0)
+        {
+            foreach (Renderer r in rends)
+                r.enabled = true;
+        }
+    }
+}
