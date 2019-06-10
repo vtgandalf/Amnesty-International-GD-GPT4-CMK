0adb53d7bff98695c0fe934a67293c81955d6feb
Valeri Todorov <37157239+vtgandalf@users.noreply.github.com>
2019-06-10 21:39:22 +0200

    NPCs
    + prefabs
    + animations
    + sprites
    + movement

---
+++ Amnesty International Group 2/Assets/Scripts/NPCMovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovementController : MonoBehaviour
+{
+    private NPCMovement movement;
+    public Vector2 locationToGoTo;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = gameObject.GetComponent<NPCMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GoTo(locationToGoTo);
+        GoTo(player.transform.position);
+    }
+
+    private void GoTo(Vector2 target)
+    {
+        Vector3 currentPosition = gameObject.transform.position;
+        float x = target.x-currentPosition.x;
+        float y = target.y-currentPosition.y;
+        if(x!=0 || y!=0)
+        {
+            movement.UpdateHorizontal(x);
+            movement.UpdateVertical(y);
+        }
+    }
+}
