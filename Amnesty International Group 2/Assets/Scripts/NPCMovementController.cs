01449a09ef75f93d2784413da89d5b2d1378211e
Valeri Todorov <37157239+vtgandalf@users.noreply.github.com>
2019-06-14 09:16:52 +0200

    Markers NPC movement
    + now you can drop in a game object containing the markers and it will parse them to the NPC

--- Amnesty International Group 2/Assets/Scripts/NPCMovementController.cs
+++ Amnesty International Group 2/Assets/Scripts/NPCMovementController.cs
@@ -6,19 +6,34 @@ public class NPCMovementController : MonoBehaviour
 {
     private NPCMovement movement;
     public Vector2 locationToGoTo;
-    public List<GameObject> markers;
+    public GameObject MarkersContainer;
+    private List<GameObject> markers = new List<GameObject>();
     int position = 0;
     bool previousTargetReached = false;
     // Start is called before the first frame update
     void Start()
     {
         movement = gameObject.GetComponent<NPCMovement>();
+        if(MarkersContainer!=null)
+        {
+            Transform par = MarkersContainer.transform;
+            int childCnt = par.childCount;
+            for (int i = 0; i < childCnt; i++)
+            {
+                Transform childX = par.GetChild(i);
+                markers.Add(childX.gameObject);
+                if (childX == transform)
+                {
+                    break;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(markers.Count!=0)
+        if (markers.Count != 0)
         {
             FollowMarkers();
         }
@@ -27,26 +42,25 @@ public class NPCMovementController : MonoBehaviour
     private void FollowMarkers()
     {
         GoTo(markers[position].transform.position);
-        if(previousTargetReached != movement.TargetHasBeenReached)
+        if (previousTargetReached != movement.TargetHasBeenReached)
         {
             previousTargetReached = movement.TargetHasBeenReached;
-            if(movement.TargetHasBeenReached)
+            if (movement.TargetHasBeenReached)
             {
                 position++;
-                if(position>markers.Count-1)
+                if (position > markers.Count - 1)
                 {
                     position = 0;
                 }
-                Debug.Log(position);
             }
         }
     }
     private void GoTo(Vector2 target)
     {
         Vector3 currentPosition = gameObject.transform.position;
-        float x = target.x-currentPosition.x;
-        float y = target.y-currentPosition.y;
-        if(x!=0 || y!=0)
+        float x = target.x - currentPosition.x;
+        float y = target.y - currentPosition.y;
+        if (x != 0 || y != 0)
         {
             movement.UpdateHorizontal(x);
             movement.UpdateVertical(y);
